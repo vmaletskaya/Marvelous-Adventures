f6a3f02447c6ab8d5058d1ccc4dacef24e863af0
Vika Maletskaya <v.maletskayaah@gmail.com>
2024-02-01 18:33:42 +0300

    added logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Logo from 'elements/Logo/Logo';
+
+
+const Header = () => {
+    return (
+    <Logo />  
+    )
+}
+
+
+export default Header;
---
+++ src/elements/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import  logo  from '../../images/logo.svg'
+
+const Logo = () => {
+    return (
+         <img src= {logo} alt = 'logo' />
+    )
+}
+
+
+export default Logo;
