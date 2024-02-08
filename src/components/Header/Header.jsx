c3632e4438ce067e17bcee8fe7fdeb451061cc92
Vika Maletskaya <v.maletskayaah@gmail.com>
2024-02-08 16:58:30 +0300

    added something to hero

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import Logo from 'elements/Logo/Logo';
 import SearchBar from 'elements/SearchBar/SearchBar';
 import css from './Header.module.css'
 
+
 const Header = () => {
   return (
     <div className={css.header}>
--- src/pages/HomePage/Hero.jsx
+++ src/pages/HomePage/Hero.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
+
 import css from './Hero.module.css';
+
 import MarvelousHero from '../../images/pantera.svg';
 import MarvelousHero1 from '../../images/pantera1.svg';
 
+
 const Hero = () => {
   return (
-    <div>
+    <div className={css.wrapper}>
+     
       <div className={css.container}>
         <p className={css.heroCaption}>Web-based platform</p>
         <h1 className={css.heroTitle}>Marvelous Adventures</h1>
@@ -15,23 +20,37 @@ const Hero = () => {
           our Characters and Comics sections and discover your new favorites
           today.
         </p>
-        <button className= {css.heroBtn}>All comics</button>
+        <button className={css.heroBtn}>All comics</button>
       </div>
       <div className={css.pagination}>
         <div className={css.pageEl}></div>
         <div className={css.pageEl}></div>
         <div className={css.pageEl}></div>
       </div>
-      <div>
-              <img src={MarvelousHero} alt="Black Panther" className={css.heroImg} />
-        <img src={MarvelousHero1} alt="Black Panther" className={css.heroImg} />
-        <p>Characters</p>
-        <p>
+      
+        <div className={css.wrapperImg}>
+        <img
+          src={MarvelousHero}
+          alt="Black Panther"
+          className={css.heroImg1}
+        />
+        
+          <img
+            src={MarvelousHero1}
+            alt="Black Panther"
+            className={css.heroImg}
+          />
+      </div>
+      <div className={css.rightWrapper}>
+         <p className= {css.imgCaption}>Characters</p>
+        <p className={css.imgDesc}>
           T’Challa is the king of the secretive and highly advanced African
           nation of Wakanda - as well as the powerful warrior known as the Black
           Panther.
         </p>
       </div>
+       
+      
     </div>
   );
 };
