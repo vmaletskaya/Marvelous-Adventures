4dc4b3221a6db86a2e01e90c2e18e8eb87c56f60
Vika Maletskaya <v.maletskayaah@gmail.com>
2024-03-07 13:49:01 +0300

    updated LastComics

--- src/elements/LastComics/LastComics.jsx
+++ src/elements/LastComics/LastComics.jsx
@@ -1,29 +1,40 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import ComicsCard from 'elements/ComicsCard/ComicsCard';
+import fetchComics from '../../helpers/api'; 
 
 import css from './LastComics.module.css';
 
-const LastComics = ({ comics, onItemClick }) => {
-  if (!comics || !Array.isArray(comics)) {
-    return null; 
-  }
+const LastComics = ({ onItemClick }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchComics()
+      .then(data => setItems(data.results)) 
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div>
-        <h2 className={css.titleBlock}>Last Comics</h2>
-       <ul className={css.comicsList}>
-      {comics.map(comic => (
+    <div className={css.container}>
+       <h2 className={css.titleBlock}>Last Comics</h2>
+      <ul className={css.comicsList}>
+      {items.map(item => (
         <ComicsCard
-          key={comic.id} 
-          image={comic.thumbnail} 
-          title={comic.title} 
-          authors={comic.authors}
-          onClick={() => onItemClick(comic)} 
+          key={item.id}
+          image={`${item.thumbnail.path}.${item.thumbnail.extension}`} 
+          title={item.title}
+          authors={item.creators.items.map(creator => creator.name)}
+          onClick={() => onItemClick(item)}
         />
       ))}
     </ul>
     </div>
-   
+    
   );
 };
 
 export default LastComics;
+
+
+
+
