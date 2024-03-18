0e61c9f72a524f27201761be9ce790bb342586dd
Vika Maletskaya <v.maletskayaah@gmail.com>
2024-03-18 16:34:53 +0300

    fix license

--- src/components/Footer/License/License.jsx
+++ src/components/Footer/License/License.jsx
@@ -3,7 +3,7 @@ import css from '../Footer.module.css';
 const License = () => {
   return (
     <div className={css.liscenceBlock}>
-      <a className={css.link} href="https://https://github.com/vmaletskaya" target="_blank" rel="noopener noreferrer" title="Developer's page">
+      <a className={css.link} href="https://github.com/vmaletskaya" target="_blank" rel="noopener noreferrer" title="Developer's page">
       <h4 className={css.liscence}>Privacy Policy Public Offering Agreement</h4>
       </a>
     </div>
